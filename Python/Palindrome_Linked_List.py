9d033ca5cae56944f9f53d27a22e53d130887922
Your Name <sujshrest@gmail.com>
2025-06-01 19:22:39 +0530

    Python

---
+++ Python/Palindrome_Linked_List.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next is not None:
+            current = current.next
+
+        current.next = new_node
+
+    def is_palindrome(self):
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        node = None
+        while slow:
+            temp = slow.next
+            slow.next = node
+            node = slow
+            slow = temp
+
+        first, second = self.head, node
+
+        while second:
+            if first.data != second.data:
+                return False
+            first = first.next
+            second = second.next
+
+        return True
+
+n = int(input())
+elements = map(int, input().split())
+
+ll = LinkedList()
+for ele in elements:
+    ll.insert(ele)
+
+print(ll.is_palindrome())
